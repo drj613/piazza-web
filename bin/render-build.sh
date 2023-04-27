7f1c96866a973c06b48663da331756d66731d051
DJ <30848200+drj613@users.noreply.github.com>
2023-04-27 10:39:55 -0400

    add rubocop and set up deployment files for Render.com

---
+++ bin/render-build.sh
@@ -0,0 +1,6 @@
+# build script for Render.com
+#!/usr/bin/env bash # exit on error set -o errexit
+  bundle install
+  bundle exec rake assets:precompile
+  bundle exec rake assets:clean
+  bundle exec rake db:migrate
